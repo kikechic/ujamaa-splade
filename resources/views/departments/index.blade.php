914910bede10010f2c53c5b553c47ae08b2b689f
kikechic <96387806+kikechic@users.noreply.github.com>
2023-11-05 20:38:50 +0300

    Initial commit

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -30,7 +30,27 @@
 			<x-splade-cell status>
 				<x-status :active="$item->status" />
 			</x-splade-cell>
-			<x-splade-cell actions></x-splade-cell>
+			<x-splade-cell actions>
+				@can('departments_update')
+					<x-splade-link
+						class="mr-2 text-blue-600 hover:underline"
+						:href="route('departments.edit', $item)"
+						modal
+					>
+						{{ __('Edit') }}
+					</x-splade-link>
+				@endcan
+				@can('departments_delete')
+					<x-splade-link
+						class="text-red-600 hover:underline"
+						method="DELETE"
+						confirm
+						:href="route('departments.destroy', $item)"
+					>
+						{{ __('Delete') }}
+					</x-splade-link>
+				@endcan
+			</x-splade-cell>
 		</x-splade-table>
 	</x-panel>
 </x-app-layout>
--- resources/views/designations/index.blade.php
+++ resources/views/designations/index.blade.php
@@ -30,7 +30,27 @@
 			<x-splade-cell status>
 				<x-status :active="$item->status" />
 			</x-splade-cell>
-			<x-splade-cell actions></x-splade-cell>
+			<x-splade-cell actions>
+				@can('designations_update')
+					<x-splade-link
+						class="mr-2 text-blue-600 hover:underline"
+						:href="route('designations.edit', $item)"
+						modal
+					>
+						{{ __('Edit') }}
+					</x-splade-link>
+				@endcan
+				@can('designations_delete')
+					<x-splade-link
+						class="text-red-600 hover:underline"
+						method="DELETE"
+						confirm
+						:href="route('designations.destroy', $item)"
+					>
+						{{ __('Delete') }}
+					</x-splade-link>
+				@endcan
+			</x-splade-cell>
 		</x-splade-table>
 	</x-panel>
 </x-app-layout>
