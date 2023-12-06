bc873891b24fc30376fa5bd0c0323ccfa79c3203
kikechic <96387806+kikechic@users.noreply.github.com>
2023-12-06 09:14:23 +0300

    Initial commit

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,17 +1,17 @@
 <div
 	class="sticky top-0 h-16 min-h-[4rem] w-full justify-center border-slate-200 bg-slate-50 py-3 pt-4 shadow-md"
 >
-	<nav class="container px-4 mx-auto">
+	<nav class="container mx-auto px-4">
 		<div class="flex flex-row justify-between">
-			<div class="w-10 ml-10">
+			<div class="ml-10 w-10">
 				<x-splade-link
 					class=""
 					href="#sidebar-modal"
 				>
-					<x-lucide-menu class="w-6 h-6" />
+					<x-lucide-menu class="h-6 w-6" />
 				</x-splade-link>
 			</div>
-			<div class="inline-flex items-center w-48 px-2">
+			<div class="inline-flex w-48 items-center px-2">
 				<x-splade-state>
 					<x-splade-form
 						method="PUT"
@@ -31,6 +31,39 @@
 						</x-splade-select>
 					</x-splade-form>
 				</x-splade-state>
+			</div>
+			<div class="w-10">
+				<Dropdown>
+					<template #trigger>
+						<button
+							class="h-8 w-8 rounded-full bg-primary-500 text-base uppercase text-white"
+							type="button"
+						>
+							{{ substr(auth()->user()->name, 0, 1) }}
+						</button>
+					</template>
+					<div
+						class="flex flex-col justify-start gap-3 whitespace-nowrap rounded-md bg-white p-3 shadow-md"
+						role="menu"
+					>
+						<div
+							class="px-2 py-2 uppercase hover:cursor-pointer hover:rounded-md hover:bg-gray-100"
+						>
+							{{ auth()->user()->name }}
+						</div>
+						<div
+							class="px-2 py-2 uppercase hover:cursor-pointer hover:rounded-md hover:bg-gray-100"
+						>
+							<x-splade-link
+								:href="route('logout')"
+								method="POST"
+								confirm
+							>
+								{{ __('Logout') }}
+							</x-splade-link>
+						</div>
+					</div>
+				</Dropdown>
 			</div>
 		</div>
 	</nav>
