d190ecdd9a82a35580ab9d3377cd9cccfabe7da7
kikechic <96387806+kikechic@users.noreply.github.com>
2024-01-19 00:49:29 +0300

    Initial commit

--- resources/views/companies/partial.blade.php
+++ resources/views/companies/partial.blade.php
@@ -10,32 +10,19 @@
 			<x-slot:label>
 				{{ __('Company Logo') }}
 			</x-slot:label>
-			<x-splade-file
-				name="logo"
-				accept="image/png"
-				max-size="5MB"
-				filepond
-				preview
-			/>
+			<x-splade-file name="logo" accept="image/png" max-size="5MB" filepond preview credits="false" />
 		</x-form-line-item>
 		<x-form-line-item>
 			<x-slot:label>
 				{{ __('Timezone') }}
 			</x-slot:label>
-			<x-splade-select
-				name="timezone"
-				:options="$timezones"
-				choices
-			/>
+			<x-splade-select name="timezone" :options="$timezones" choices />
 		</x-form-line-item>
 		<x-form-line-item>
 			<x-slot:label>
 				{{ __('Status') }}
 			</x-slot:label>
-			<x-splade-select
-				name="status"
-				:options="[1 => 'Active', 0 => 'Inactive']"
-			/>
+			<x-splade-select name="status" :options="[1 => 'Active', 0 => 'Inactive']" />
 		</x-form-line-item>
 	</div>
 </div>
