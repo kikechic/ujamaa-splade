aa782fa21a23de5451bf0ed409320f180a6fb08d
kikechic <96387806+kikechic@users.noreply.github.com>
2024-01-19 01:22:28 +0300

    Initial commit

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col h-screen w-screen font-normal bg-slate-100">
 	<x-navigation />
 	<div class="flex flex-row h-screen overflow-hidden">
-		<div class="hidden lg:block h-[calc(100vh_-_8rem)] overflow-y-auto shadow-md bg-slate-50 pt-5">
+		<div class="hidden lg:block h-[calc(100vh_-_8rem)] w-64 overflow-y-auto shadow-md bg-slate-50 pt-5">
 			<x-sidebar />
 		</div>
 		<x-splade-modal name="sidebar-modal" slideover position="left">
