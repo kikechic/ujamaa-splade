5445bdd20284662bb778b6694aaa0e2871324e3f
kikechic <96387806+kikechic@users.noreply.github.com>
2024-02-28 04:45:34 +0300

    Initial commit

--- app/Http/Controllers/TimesheetController.php
+++ app/Http/Controllers/TimesheetController.php
@@ -25,7 +25,7 @@ class TimesheetController extends Controller
 {
     public function index()
     {
-        dd(auth()->user()->permissions, auth()->user()->roles);
+        dd(auth()->user()->permissions->toArray(), auth()->user()->roles->toArray(), Gate::abilities());
         abort_unless(Gate::allows('timesheets_access'), Response::HTTP_FORBIDDEN, 'You are not authorised to access timesheets');
 
         return view('timesheets.index', [
